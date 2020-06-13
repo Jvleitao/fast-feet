20e4119d0a65d3504a9b66898a21dcb3b2926dfa
Joao Vitor <jvcleitao@gmail.com>
2020-06-13 12:49:55 -0300

    config multer to add files

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -17,6 +21,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/recipient', RecipientController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/deliveryman', DeliverymanController.store);
 export default routes;
