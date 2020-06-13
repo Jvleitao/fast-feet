77a5f8064abe1b7ab4a2278bd85407c50027e122
Joao Vitor <jvcleitao@gmail.com>
2020-06-13 11:43:33 -0300

    create DeliverymanController whit store method and add to routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,4 +17,6 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/recipient', RecipientController.store);
+
+routes.post('/deliveryman', DeliverymanController.store);
 export default routes;
